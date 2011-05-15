8fda719adbf67cd2ccd4806b65e6af25c64e2250
cipolini <andrecipolini@gmail.com>
2011-05-15 13:08:26 -0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,35 +6,36 @@
 #   cities = City.create([#{ :name => 'Chicago' }, #{ :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-#EstadoCivil.create([
-#{:descricao => 'Solteiro(a)'},
-#{:descricao => 'Casado(a)'},
-#{:descricao => 'Viuvo(a)'}
-#])
+EstadoCivil.create([
+{:descricao => 'Solteiro(a)'},
+{:descricao => 'Casado(a)'},
+{:descricao => 'Viuvo(a)'}
+])
 
-#TipoSystemUser.create([
-#{:descricao => 'Adm'},
-#{:descricao => 'Medico'},
-#{:descricao => 'Secretaria'}
-#])
+TipoSystemUser.create([
+{:descricao => 'Adm'},
+{:descricao => 'Medico'},
+{:descricao => 'Secretaria'}
+])
 
-#StatusSystemUser.create([
-#{:descricao => 'Ativo'},
-#{:descricao => 'Inativo'}
-#])
+StatusSystemUser.create([
+{:descricao => 'Ativo'},
+{:descricao => 'Inativo'}
+])
 
-#Convenio.create([
-#{:descricao => 'Porto Seguro'}
-#])
+Convenio.create([
+{:descricao => 'Porto Seguro'}
+])
 
-#Plano.create([
-#{:descricao => 'BASICO EMPRESARIAL'}
-#])
+Plano.create([
+{:descricao => 'BASICO EMPRESARIAL'}
+])
 
-#SystemUser.create([
-#{:login => 'adm_cipolini', :password => 'T2m7*321', :email => 'andrecipolini@gmail.com', :status => 'Ativo', :tipo => '1'},
-#{:login => 'medico', :password => '123', :email => 'medico@dr.com', :status => 'Ativo', :tipo => '2'}
-#])
+SystemUser.create([
+{:login => 'adm_cipolini', :password => 'T2m7*321', :email => 'andrecipolini@gmail.com', :status => 'Ativo', :tipo => '1'},
+{:login => 'zebio', :password => '123', :email => 'medico@dr.com', :status => 'Ativo', :tipo => '3'},
+{:login => 'medico', :password => '123', :email => 'medico@dr.com', :status => 'Ativo', :tipo => '2'}
+])
 
 TipoConsulta.create([
 {:descricao => 'Avaliacao', :status => 'Ativo'},
